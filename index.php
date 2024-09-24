f5abca0325d72ce63a2913a4de319c44a2ff321e
Drew Maliy <drew.maliy@speedandfunction.com>
2024-09-24 18:32:38 +0300

    Update

--- index.php
+++ index.php
@@ -330,7 +330,8 @@
     </div>
     <div class="description text-center">
       <p style="font-size: 16px;">
-        Hi! My name is Andriy Malyeyev. I am CEO & Founder at <a href="https://shyk.systems">ShykSystems</a><br>
+        Hi! My name is Andriy Malyeyev.<br>
+        I am CEO & Founder at <a href="https://shyk.systems">ShykSystems</a><br>
         Also, I live in Kyiv, Ukraine 🇺🇦<br>
         Feel free ping me in these resources below.
       </p>
